fc175512578c4be8c3f5dd54a3e8f73224bb1c3f
Olaf Garleja <o.garleja@gmail.com>
2020-06-29 22:08:36 +0200

    added build

---
+++ build/precache-manifest.767bee2d32249aaa2884e5f5a9876500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d54f02e02a1ef7b511265c5cbc85c5",
+    "url": "/signup/index.html"
+  },
+  {
+    "revision": "0014a650248dd1e20130",
+    "url": "/signup/static/css/main.23f2df4d.chunk.css"
+  },
+  {
+    "revision": "a4a58ad7d3a5566d3e4e",
+    "url": "/signup/static/js/2.67e166b3.chunk.js"
+  },
+  {
+    "revision": "cc539bb4a85cbe0e42d76df177ff76fc",
+    "url": "/signup/static/js/2.67e166b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0014a650248dd1e20130",
+    "url": "/signup/static/js/main.29d96fc9.chunk.js"
+  },
+  {
+    "revision": "780b7e99406feabdb6a8",
+    "url": "/signup/static/js/runtime-main.21dbf17d.js"
+  }
+]);
